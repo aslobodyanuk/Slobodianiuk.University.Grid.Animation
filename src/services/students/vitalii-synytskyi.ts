ccb132449dce43bdddcb2f321f7106de61a45772
vitaliy <howery1337@gmail.com>
2023-03-19 16:12:53 +0200

    change animation name

--- src/services/students/vitalii-synytskyi.ts
+++ src/services/students/vitalii-synytskyi.ts
@@ -20,30 +20,89 @@ export class VitaliiSynytskyiConfigLoaderService implements IConfigLoaderService
             name: 'Vitalii',
             surname: 'Synytskyi',
             email: this.getStudentEmail(),
-            projectName: 'Circle',
+            projectName: 'Cactus',
             personalProjectLink: 'https://witty-plant-0f1aca810.2.azurestaticapps.net',
-            header: getDefaultCircleFrame(0),
-            frames: frames()
+            header: Constants.DefaultConfig.header,
+            frames: generateFrames()
         }
     }
 }
-export const getDefaultCircleFrame = (frameNumber: number): IFrame => {
-    let result: IFrame = {
-        frameNumber: frameNumber,
-        pixels: []
-    };
-
-
-
-    return result;
-}
-
-
-
-
-function frames(): IFrame[] {
+function generateFrames(): IFrame[] {
     const frames: IFrame[] = [];
 
+    const firstFrame: IPixelState[] = [
+        { x: 9, y: 1, color: Colour.White }, { x: 9, y: 2, color: Colour.White }, { x: 9, y: 3, color: Colour.White }, { x: 9, y: 4, color: Colour.White }, { x: 9, y: 5, color: Colour.White }, { x: 9, y: 6, color: Colour.White }, { x: 9, y: 7, color: Colour.White }, { x: 9, y: 8, color: Colour.White },
+        { x: 9, y: 9, color: Colour.White }, { x: 9, y: 10, color: Colour.White }, { x: 9, y: 11, color: Colour.White }, { x: 9, y: 12, color: Colour.White }, { x: 9, y: 13, color: Colour.White }, { x: 9, y: 14, color: Colour.White }, { x: 9, y: 15, color: Colour.White }, 
+        { x: 10, y: 0, color: Colour.White }, { x: 10, y: 15, color: Colour.White }, 
+        { x: 11, y: 1, color: Colour.White }, { x: 11, y: 2, color: Colour.White }, { x: 11, y: 3, color: Colour.White }, { x: 11, y: 4, color: Colour.White }, { x: 11, y: 5, color: Colour.White }, { x: 11, y: 6, color: Colour.White }, { x: 11, y: 7, color: Colour.White }, { x: 11, y: 8, color: Colour.White },
+        { x: 11, y: 9, color: Colour.White }, { x: 11, y: 10, color: Colour.White }, { x: 11, y: 11, color: Colour.White }, { x: 11, y: 12, color: Colour.White }, { x: 11, y: 13, color: Colour.White }, { x: 11, y: 14, color: Colour.White }, { x: 11, y: 15, color: Colour.White },
+        { x: 12, y: 3, color: Colour.White }, { x: 12, y: 5, color: Colour.White }, { x: 12, y: 10, color: Colour.White }, { x: 12, y: 12, color: Colour.White }, 
+        { x: 13, y: 3, color: Colour.White }, { x: 13, y: 5, color: Colour.White }, { x: 13, y: 10, color: Colour.White }, { x: 13, y: 12, color: Colour.White }, 
+        { x: 14, y: 3, color: Colour.White }, { x: 14, y: 5, color: Colour.White }, { x: 14, y: 10, color: Colour.White }, { x: 14, y: 12, color: Colour.White }, 
+        { x: 15, y: 4, color: Colour.White }, { x: 15, y: 5, color: Colour.White }, { x: 15, y: 6, color: Colour.White }, { x: 15, y: 7, color: Colour.White }, { x: 15, y: 8, color: Colour.White }, { x: 15, y: 9, color: Colour.White }, { x: 15, y: 10, color: Colour.White }, { x: 15, y: 11, color: Colour.White }, 
+    ];
+
+    const secondFrame: IPixelState[] = [
+        { x: 4, y: 6, color: Colour.White }, { x: 4, y: 7, color: Colour.White }, { x: 4, y: 8, color: Colour.White }, { x: 4, y: 9, color: Colour.White }, 
+        { x: 5, y: 5, color: Colour.White }, { x: 5, y: 10, color: Colour.White }, 
+        { x: 6, y: 4, color: Colour.White }, { x: 6, y: 8, color: Colour.White }, { x: 6, y: 11, color: Colour.White }, 
+        { x: 7, y: 4, color: Colour.White }, { x: 7, y: 6, color: Colour.White }, { x: 7, y: 11, color: Colour.White }, 
+        { x: 8, y: 4, color: Colour.White }, { x: 8, y: 9, color: Colour.White }, { x: 8, y: 11, color: Colour.White }, 
+        { x: 9, y: 1, color: Colour.White }, { x: 9, y: 2, color: Colour.White }, { x: 9, y: 3, color: Colour.White }, { x: 9, y: 4, color: Colour.White }, { x: 9, y: 5, color: Colour.White }, { x: 9, y: 6, color: Colour.White }, { x: 9, y: 7, color: Colour.White }, { x: 9, y: 8, color: Colour.White },
+        { x: 9, y: 9, color: Colour.White }, { x: 9, y: 10, color: Colour.White }, { x: 9, y: 11, color: Colour.White }, { x: 9, y: 12, color: Colour.White }, { x: 9, y: 13, color: Colour.White }, { x: 9, y: 14, color: Colour.White }, { x: 9, y: 15, color: Colour.White },
+        { x: 10, y: 0, color: Colour.White }, { x: 10, y: 15, color: Colour.White },
+        { x: 11, y: 1, color: Colour.White }, { x: 11, y: 2, color: Colour.White }, { x: 11, y: 3, color: Colour.White }, { x: 11, y: 4, color: Colour.White }, { x: 11, y: 5, color: Colour.White }, { x: 11, y: 6, color: Colour.White }, { x: 11, y: 7, color: Colour.White }, { x: 11, y: 8, color: Colour.White },
+        { x: 11, y: 9, color: Colour.White }, { x: 11, y: 10, color: Colour.White }, { x: 11, y: 11, color: Colour.White }, { x: 11, y: 12, color: Colour.White }, { x: 11, y: 13, color: Colour.White }, { x: 11, y: 14, color: Colour.White }, { x: 11, y: 15, color: Colour.White },
+        { x: 12, y: 3, color: Colour.White }, { x: 12, y: 5, color: Colour.White }, { x: 12, y: 10, color: Colour.White }, { x: 12, y: 12, color: Colour.White },
+        { x: 13, y: 3, color: Colour.White }, { x: 13, y: 5, color: Colour.White }, { x: 13, y: 10, color: Colour.White }, { x: 13, y: 12, color: Colour.White },
+        { x: 14, y: 3, color: Colour.White }, { x: 14, y: 5, color: Colour.White }, { x: 14, y: 10, color: Colour.White }, { x: 14, y: 12, color: Colour.White },
+        { x: 15, y: 4, color: Colour.White }, { x: 15, y: 5, color: Colour.White }, { x: 15, y: 6, color: Colour.White }, { x: 15, y: 7, color: Colour.White }, { x: 15, y: 8, color: Colour.White }, { x: 15, y: 9, color: Colour.White }, { x: 15, y: 10, color: Colour.White }, { x: 15, y: 11, color: Colour.White }, 
+    ];
+
+    const thirdFrame: IPixelState[] = [
+        { x: 0, y: 6, color: Colour.White }, { x: 0, y: 7, color: Colour.White }, { x: 0, y: 8, color: Colour.White }, { x: 0, y: 9, color: Colour.White },
+        { x: 1, y: 5, color: Colour.White }, { x: 1, y: 10, color: Colour.White }, { x: 1, y: 12, color: Colour.White },
+        { x: 2, y: 4, color: Colour.White }, { x: 2, y: 8, color: Colour.White }, { x: 2, y: 11, color: Colour.White }, { x: 2, y: 13, color: Colour.White },
+        { x: 3, y: 4, color: Colour.White }, { x: 3, y: 6, color: Colour.White }, { x: 3, y: 11, color: Colour.White }, { x: 3, y: 12, color: Colour.White },
+        { x: 4, y: 4, color: Colour.White }, { x: 4, y: 9, color: Colour.White }, { x: 4, y: 11, color: Colour.White },
+        { x: 5, y: 3, color: Colour.White }, { x: 5, y: 4, color: Colour.White }, { x: 5, y: 8, color: Colour.White }, { x: 5, y: 11, color: Colour.White }, { x: 5, y: 12, color: Colour.White }, { x: 5, y: 13, color: Colour.White },
+        { x: 6, y: 2, color: Colour.White }, { x: 6, y: 4, color: Colour.White }, { x: 6, y: 6, color: Colour.White }, { x: 6, y: 11, color: Colour.White }, { x: 6, y: 14, color: Colour.White },
+        { x: 7, y: 3, color: Colour.White }, { x: 7, y: 4, color: Colour.White }, { x: 7, y: 9, color: Colour.White }, { x: 7, y: 11, color: Colour.White }, { x: 7, y: 12, color: Colour.White }, { x: 7, y: 13, color: Colour.White },
+        { x: 8, y: 4, color: Colour.White }, { x: 8, y: 11, color: Colour.White },
+        { x: 9, y: 1, color: Colour.White }, { x: 9, y: 2, color: Colour.White }, { x: 9, y: 3, color: Colour.White }, { x: 9, y: 4, color: Colour.White }, { x: 9, y: 5, color: Colour.White }, { x: 9, y: 6, color: Colour.White }, { x: 9, y: 7, color: Colour.White }, { x: 9, y: 8, color: Colour.White },
+        { x: 9, y: 9, color: Colour.White }, { x: 9, y: 10, color: Colour.White }, { x: 9, y: 11, color: Colour.White }, { x: 9, y: 12, color: Colour.White }, { x: 9, y: 13, color: Colour.White }, { x: 9, y: 14, color: Colour.White }, { x: 9, y: 15, color: Colour.White },
+        { x: 10, y: 0, color: Colour.White }, { x: 10, y: 15, color: Colour.White },
+        { x: 11, y: 1, color: Colour.White }, { x: 11, y: 2, color: Colour.White }, { x: 11, y: 3, color: Colour.White }, { x: 11, y: 4, color: Colour.White }, { x: 11, y: 5, color: Colour.White }, { x: 11, y: 6, color: Colour.White }, { x: 11, y: 7, color: Colour.White }, { x: 11, y: 8, color: Colour.White },
+        { x: 11, y: 9, color: Colour.White }, { x: 11, y: 10, color: Colour.White }, { x: 11, y: 11, color: Colour.White }, { x: 11, y: 12, color: Colour.White }, { x: 11, y: 13, color: Colour.White }, { x: 11, y: 14, color: Colour.White }, { x: 11, y: 15, color: Colour.White },
+        { x: 12, y: 3, color: Colour.White }, { x: 12, y: 5, color: Colour.White }, { x: 12, y: 10, color: Colour.White }, { x: 12, y: 12, color: Colour.White },
+        { x: 13, y: 3, color: Colour.White }, { x: 13, y: 5, color: Colour.White }, { x: 13, y: 10, color: Colour.White }, { x: 13, y: 12, color: Colour.White },
+        { x: 14, y: 3, color: Colour.White }, { x: 14, y: 5, color: Colour.White }, { x: 14, y: 10, color: Colour.White }, { x: 14, y: 12, color: Colour.White },
+        { x: 15, y: 4, color: Colour.White }, { x: 15, y: 5, color: Colour.White }, { x: 15, y: 6, color: Colour.White }, { x: 15, y: 7, color: Colour.White }, { x: 15, y: 8, color: Colour.White }, { x: 15, y: 9, color: Colour.White }, { x: 15, y: 10, color: Colour.White }, { x: 15, y: 11, color: Colour.White }, 
+    ];
+
+
+    for (let frameNumber = 0; frameNumber < Constants.MaxTotalFrames; frameNumber++) {
+        const pixels: IPixelState[] = [];
+
+        const show1 = frameNumber % 30 < 10;
+        const show2 = frameNumber % 30 >= 10 && frameNumber % 30 < 20;
+        const show3 = frameNumber % 30 >= 20;
+
+        if (show1) {
+            pixels.push(...firstFrame.map(pixel => ({ ...pixel, x: pixel.x, color: Colour.White })));
+        }
+        if (show2) {
+            pixels.push(...secondFrame.map(pixel => ({ ...pixel, x: pixel.x, color: Colour.White })));
+        }
+        if (show3) {
+            pixels.push(...thirdFrame.map(pixel => ({ ...pixel, x: pixel.x, color: Colour.White })));
+        }
+
+        frames.push({
+            frameNumber: frameNumber,
+            pixels: pixels,
+        });
+    }
 
     return frames;
 }
